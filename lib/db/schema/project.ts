4a86d3172de598dafa9d5d82d8dc9fbc89b8291c
Louis Le Doussal <152080571+Louis3133@users.noreply.github.com>
2026-02-24 11:10:10 +0100

    ajoute le système d'authentification (#4)

--- lib/db/schema/project.ts
+++ lib/db/schema/project.ts
@@ -1,6 +1,8 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { user } from "./auth";
 
 export const projects = sqliteTable("projects", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
+  userId: int().notNull().references(() => user.id),
 });
---
+++ pages/sign-out.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div>
+    <span v-if="authStore.loading" class="loading loading-spinner loading-xl" />
+    Yeah
+  </div>
+</template>
